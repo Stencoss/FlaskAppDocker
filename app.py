19a98c29ee0eef093d11d91f0e90cf248b8de30a
StevenNimble <stadenoia@gmail.com>
2022-07-25 07:46:09 -0500

    updated file structure

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 # Main file
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
     
+@app.route('/secondPage')
+def secondPage():
+    return render_template('secondPage.html')
+
+
+if __name__ == '__main__': 
+    app.run(debug=True)
